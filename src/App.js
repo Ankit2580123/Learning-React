e4e26208c150c82c3e8b98a7f3aef56d3b3e56a3
Ankit Kumar <ankitkumar2580123@gmail.com>
2023-11-03 00:17:59 +0530

    Learn Usesate and Handlingevents Through Projects

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
    <>
 
-   <Navbar title="This is Title" about="This is About" contact={7903493441} />
+   {/* <Navbar title="This is Title" about="This is About" contact={7903493441} /> */}
    {/* //this is for default */}
    {/* <Navbar/> */}
+   <div className="container my-3" id='textForms'>
+   <h1>Enter the text here</h1>
+      <TextForm/>
+   </div>
+   
    </>
   );
 }
---
+++ src/components/TextForm.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react'
+
+export default function TextForm(props) {
+                    const handleUpperCase=()=>{
+
+                                      let newText=text.toUpperCase();
+                                      setText(newText);
+                    }
+                    const handleLowerCase=()=>{
+
+                                      let newText=text.toLowerCase();
+                                      setText(newText);
+                    }
+                    const handleChange=(event)=>{
+                                       setText(event.target.value);
+                    }
+                    const [text,setText]=useState("Enter Text Here");
+                    let word=text.split(" ");
+              
+                    let newCleanWord=word.filter(function(elm){
+                              return elm!=" ";
+                    });
+  return (
+    <>
+     <div>
+                   <div className="mb-3">
+                   <textarea rows="12" className="form-control" value={text} onChange={handleChange}></textarea>
+                   </div>
+                   <button className='btn btn-primary mx-2' onClick={handleUpperCase}>Convert to Uppercase</button>
+                   <button className='btn btn-primary mx-2' onClick={handleLowerCase}>Convert to Lowercase</button>
+   </div>
+
+    <div className="container my-3">
+      <h2>Your Text Summary</h2>
+      <p>{text.trim().length} Characters Including Spaces</p>
+      <p>{newCleanWord.length-1} Words</p>
+    </div>
+    </>
+   
+
+  )
+}
